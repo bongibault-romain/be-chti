e292c60591d42bf1440f9e33d26e49b2df90cce9
Bongibault Romain <bongibault.romain@gmail.com>
2024-03-15 09:21:17 +0100

    feat: initial commit

---
+++ Principal/principal.c
@@ -0,0 +1,35 @@
+
+
+#include "DriverJeuLaser.h"
+#include "ServiceJeuLaser.h"
+
+
+
+int main(void)
+{
+
+// ===========================================================================
+// ============= INIT PERIPH (faites qu'une seule fois)  =====================
+// ===========================================================================
+
+/* Après exécution : le coeur CPU est clocké à 72MHz ainsi que tous les timers */
+CLOCK_Configure();
+
+/* Configuration du son (voir ServiceJeuLaser.h) 
+ Insérez votre code d'initialisation des parties matérielles gérant le son ....*/	
+
+
+	
+	
+
+//============================================================================	
+	
+	
+while	(1)
+	{
+	}
+}
+
+
+
+
