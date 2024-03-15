0a2ca45c739e6285c659257376c45731113b57f2
Bongibault Romain <bongibault.romain@gmail.com>
2024-03-15 10:55:43 +0100

    cree une variable (OMG)

--- GestionSon/GestionSon.s
+++ GestionSon/GestionSon.s
@@ -9,8 +9,10 @@
 
 ;Section RAM (read write):
 	area    maram,data,readwrite
-		
 
+	export GestionSon_Index
+		
+GestionSon_Index dcd 0
 	
 ; ===============================================================================================
 	
@@ -20,10 +22,14 @@
 ;Section ROM code (read only) :		
 	area    moncode,code,readonly
 ; écrire le code ici
+
 	export GestionSon_callback
 	
 GestionSon_callback
-	mov r0,r0
+	ldr r1, =GestionSon_Index
+	ldr r0,[r1]
+	add r0,#1
+	str r0,[r1]
 	bx lr
 
 
