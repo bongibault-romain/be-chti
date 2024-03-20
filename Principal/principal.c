f12853bfd5dae267b0fee8be3a86aef7360091c1
Bongibault Romain <bongibault.romain@gmail.com>
2024-03-20 17:01:33 +0100

    SUPER FINITION DU 1ER

--- Principal/principal.c
+++ Principal/principal.c
@@ -16,7 +16,7 @@ int main(void)
 /* Après exécution : le coeur CPU est clocké à 72MHz ainsi que tous les timers */
 CLOCK_Configure();
 
-ServJeuLASER_Systick_IT_Init(PeriodeSonMicroSec ,0,GestionSon_Callback);
+ServJeuLASER_Son_Init(PeriodeSonMicroSec ,0,GestionSon_Callback);
 
 /* Configuration du son (voir ServiceJeuLaser.h) 
  Insérez votre code d'initialisation des parties matérielles gérant le son ....*/	
