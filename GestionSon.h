a394b9b9feae18c016b3098836f70d0124201913
Bongibault Romain <bongibault.romain@gmail.com>
2024-04-30 18:12:58 +0200

    saucisson aux herbes

--- GestionSon.h
+++ GestionSon.h
@@ -1,3 +1,7 @@
-extern void GestionSon_Callback(void);
+#ifndef GESTIONSON_H
+#define GESTIONSON_H
 
+extern void GestionSon_Callback(void);
 extern void GestionSon_Start(void);
+
+#endif
--- Principal/principal.c
+++ Principal/principal.c
@@ -6,40 +6,71 @@
 #include "./../GestionSon.h"
 #include "./../DFT/DFT.h"
 
+#define SEUIL 0x00005FFF
+
 extern int PeriodeSonMicroSec;
 
-extern short LeSignal[64];
+static short TheSignal[64];
 
-static float dfts[64];
-static int sdfts[64];
+static int dfts[64];
+static int oldDfts[64];
+int frequencies[6] = {17,18,19,20,23,24};
+
+static unsigned int scores[6] = {0,0,0,0,0,0};
+
+void Test_Scoring(int frequencyIndex) {
+	int freq = frequencies[frequencyIndex];
+	
+	if(dfts[freq] > SEUIL && oldDfts[freq] < SEUIL){
+		GestionSon_Start();
+		scores[frequencyIndex]++;
+	}
+}
+
+void Callback() {
+	// On copie DFT dans la variable oldDFT
+	memcpy(oldDfts, dfts, 64 * sizeof(int));
+	
+	ServJeuLASER_StartDMA();
+	
+	for(int k=0;k<64;k++){
+		dfts[k] = DFT(k, TheSignal);
+	}
+	
+	for (int i = 0; i < 6; i++) {
+		Test_Scoring(i);
+	}
+}
+
+
 
 int main(void)
 {
-// ===========================================================================
-// ============= INIT PERIPH (faites qu'une seule fois)  =====================
-// ===========================================================================
+	// ===========================================================================
+	// ============= INIT PERIPH (faites qu'une seule fois)  =====================
+	// ===========================================================================
 
-/* Après exécution : le coeur CPU est clocké à 72MHz ainsi que tous les timers */
-CLOCK_Configure();
+	/* Après exécution : le coeur CPU est clocké à 72MHz ainsi que tous les timers */
+	CLOCK_Configure();
 
-ServJeuLASER_Son_Init(PeriodeSonMicroSec ,0,GestionSon_Callback);
-
+	ServJeuLASER_Son_Init(PeriodeSonMicroSec ,0,GestionSon_Callback);
+	ServJeuLASER_ADC_DMA(TheSignal);
+	ServJeuLASER_Systick_IT_Init(5000,1,Callback);
+	
+	
+//	for(int k=0;k<64;k++){
+//		dfts[k] = DFT(k, LeSignal);
+//	}
 	
 /* Configuration du son (voir ServiceJeuLaser.h) 
  Insérez votre code d'initialisation des parties matérielles gérant le son ....*/	
 	
 
 //============================================================================	
-	for(int k=0;k<64;k++){
-		sdfts[k] = DFT(k, LeSignal);
-		dfts[k] = convert(sdfts[k]);
-	}
 
 while	(1)
 	{
 	}
 }
-
-
 
 
